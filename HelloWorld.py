8cf85424e808a4c55782dcbd480659342f649e1f
palashTUT <mpalash.halder@gmail.com>
2020-05-03 13:50:34 +0100

    test motion sensor code

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,17 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import RPi.GPIO as GPIO
+import time
 
-print("hello")
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setup(3, GPIO.OUT)         #LED output pin
+
+prev_i = 0
+count = 0
+while True:
+    i=GPIO.input(12)
+    if i!=prev_i:
+        print("HEllo", count)
+        prev_i = i
+        count += 1
+    time.sleep(0.5)
